9a441f41117bfb34fa5c4bb4959c4aa6e56a2e68
Jordomav <jordomav@gmail.com>
2016-08-09 15:50:47 -0700

    write things to the Serial using C++

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,22 @@
 #include <blink.h>
 
 // int pinLed = 13
+int incomingByte;
 
 void setup () {
 
     pinMode(pinLed, OUTPUT);
+    Serial.begin(9600);
+    Serial.println('Logging to Serial');
 }
 
 void loop () {
+    
     blink();
+
+    if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.print("! ");
+    Serial.write(incomingByte);
+  }
 }
